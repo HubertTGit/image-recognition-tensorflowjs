dc1f049727a5df84cb9dd10beb06c6e3cc058360
HuberttGit <hubert.talib@gmail.com>
2024-07-03 16:33:26 +0200

    update

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,4 +5,6 @@ export const MOBILE_NET_INPUT_HEIGHT = 224;
 //for more details of the base model, visit https://www.kaggle.com/models/google/mobilenet-v3/tfJs/small-100-224-feature-vector
 export const MOBILE_NET_URL =
   'https://www.kaggle.com/models/google/mobilenet-v3/TfJs/small-100-224-feature-vector/1';
+
+export const LOCALSTORAGE = 'localstorage://mobilenet';
 export const FRAME_RATE_MS = 5;
